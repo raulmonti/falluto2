82dcbed6c18f5918d46273afd234f8400ea73a7b
raulmonti88 <raulmonti88@d936f56d-e316-d187-0cfd-378c9cfd2bb4>
2012-10-24 03:09:16 +0000

    Parser.py revisado y aceptable (seguro se ira modificando de acuerdo a las necesidades que se presenten en el checkeo semantico y de tipos, y en el compilador)

---
+++ branches/branch5/Types.py
@@ -0,0 +1,25 @@
+class Types():
+
+    # var types
+    Notype = -1
+    Int = 0
+    Bool = 1
+    Symbol = 2
+    #Reference = 3
+    
+    # fault types
+    Transient = 54
+    Stop = 55
+    Byzantine = 56
+    
+    Types = { 0:"Int"           # variables
+            , 1:"Bool"
+            , 2:"Symbol"
+            , 3:"Reference"
+            , 54:"Transient"    # faults
+            , 55:"Stop"
+            , 56:"Byzantine"
+            }
+
+    def __init__(self):
+        pass
